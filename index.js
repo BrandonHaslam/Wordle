a557508398674d48693f39866fa0b38f5fe6baa4
BrandonHaslam <BrandonHaslam@users.noreply.github.com>
2023-01-16 21:14:20 +0000

    Adds letter to grid row cell

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ function checkDarkMode () {
 // Keyboard input
 const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 const validKeys = [...alphabet];
+// This will eventually be dynamic with document.querySelectorAll(".letter-row")[index]
 const row = document.querySelector(".letter-row")
 let currentGuess = ''
 
@@ -36,13 +37,18 @@ document.addEventListener('keyup', (e) => {
     currentGuess += e.key
     console.log(currentGuess)
     console.log(row.children)
+    row.children[currentGuess.length - 1].innerText = e.key
   }
   if(key === 'Enter' && currentGuess.length === wordLength) {
     // enter function
     console.log(currentGuess)
   }
   if(key === 'Backspace' && currentGuess.length !== 0){
+    // This currently removes from the grid row but not the guess
+    console.log(currentGuess)
     currentGuess = currentGuess.slice(0, -1); 
+    console.log(currentGuess)
+    row.children[currentGuess.length - 1].innerText = ''
   }
 });
 
